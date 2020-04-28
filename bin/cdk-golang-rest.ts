fbc338da587eaab2ef828e483f94e47c3c3eebff
Shuto kikuchi <>
2020-04-28 11:16:07 +0900

    Initial commit

---
+++ bin/cdk-golang-rest.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { CdkGolangRestStack } from '../lib/cdk-golang-rest-stack';
+
+const app = new cdk.App();
+new CdkGolangRestStack(app, 'CdkGolangRestStack');
